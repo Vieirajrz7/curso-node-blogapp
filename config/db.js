6770635c6f00ea79d17f9cfca0ed4939b3f250fa
Vieirajrz7 <joaogedavim787@gmail.com>
2024-06-03 10:26:24 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production") {
     module.exports = {
-        mongoURI: "mongodb+srv://mvieirajrz:997444691Mae@@db-marcelo.dmy28a0.mongodb.net/?retryWrites=true&w=majority&appName=DB-Marcelo"
+        mongoURI: "mongodb://mvieirajrz:997444691Mae@@db-marcelo.dmy28a0.mongodb.net/?retryWrites=true&w=majority&appName=DB-Marcelo"
     }
 } else {
     module.exports = {
